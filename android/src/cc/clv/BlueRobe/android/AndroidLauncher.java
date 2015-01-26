bf54381caece95e9d92f2dc0f8fcbb355898c9c9
slightair <arksutite@gmail.com>
2015-01-26 13:07:09 +0900

    Create project

---
+++ android/src/cc/clv/BlueRobe/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package cc.clv.BlueRobe.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import cc.clv.BlueRobe.BlueRobe;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlueRobe(), config);
+	}
+}
---
+++ desktop/src/cc/clv/BlueRobe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cc.clv.BlueRobe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cc.clv.BlueRobe.BlueRobe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlueRobe(), config);
+	}
+}
