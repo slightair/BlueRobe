a68ccde37d0e08f7c66c146bce3e056cbb16e2a3
slightair <arksutite@gmail.com>
2015-04-29 16:07:36 +0900

    Add jump action

---
+++ Assets/scripts/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	private float gravity = 20f;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		moveDirection.x = -10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.isGrounded) {
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = 10f;
+			}
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
