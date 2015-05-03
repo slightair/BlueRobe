3ed0734e88b901762d3c1152216c1c9a131e461e
slightair <arksutite@gmail.com>
2015-05-04 01:02:58 +0900

    Arrangement Mushroom prefab

--- Assets/Scripts/Stage/StageLoader.cs
+++ Assets/Scripts/Stage/StageLoader.cs
@@ -12,6 +12,8 @@ namespace BlueRobe.Stage
         void Start()
         {
             map = LoadMap("debug");
+
+            ArrangeObjects();
         }
 
         // Update is called once per frame
@@ -27,6 +29,23 @@ namespace BlueRobe.Stage
             document.Load(new StringReader(stageXmlAsset.text));
 
             return new Map(document.DocumentElement);
+        }
+
+        private void ArrangeObjects()
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                for (int x = 0; x < map.width; x++)
+                {
+                    Tile tile = map.GetTile("Items", x, y);
+                    if (tile != null)
+                    {
+                        GameObject itemPrefab = (GameObject)Resources.Load("Objects/" + tile.name);
+                        Vector3 position = new Vector3(1 - y * 2, 0, -10 + x * 2);
+                        Instantiate(itemPrefab, position, Quaternion.identity);
+                    }
+                }
+            }
         }
     }
 }
