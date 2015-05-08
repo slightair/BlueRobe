2393292e1af1b42cb330cf72ca144c6ac1762b5e
slightair <arksutite@gmail.com>
2015-05-08 15:54:34 +0900

    Add character moving

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -4,6 +4,7 @@ public class CharacterControl : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private Animator animator;
+    private bool isJumpCancelled;
 
     // Use this for initialization
     void Start()
@@ -15,26 +16,59 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (isGrounded() && stateInfo.IsName("Hikari.Run"))
-            {
-                animator.SetBool("PrepareJump", true);
-            }
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            if (isGrounded())
-            {
-                animator.SetBool("PrepareJump", false);
-                rigidBody.AddForce(Vector3.up * 10, ForceMode.VelocityChange);
-            }
-        }
+        processTouches();
+
+//         Vector3 newPosition = transform.position;
+//         newPosition.x -= 0.5f;
+//         transform.position = newPosition;
     }
 
     private bool isGrounded()
     {
         return transform.position.y < 1;
+    }
+
+    private void processTouches()
+    {
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Began)
+        {
+            isJumpCancelled = false;
+            if (isGrounded() && stateInfo.IsName("Hikari.Run"))
+            {
+                animator.SetBool("PrepareJump", true);
+            }
+        }
+        else if (touch.phase == TouchPhase.Moved)
+        {
+            isJumpCancelled = true;
+            float deltaX = touch.deltaPosition.x;
+            if (deltaX != 0)
+            {
+                if (stateInfo.IsName("Hikari.PrepareJump"))
+                {
+                    animator.SetBool("PrepareJump", false);
+                }
+
+                Vector3 newPosition = transform.position;
+                newPosition.z += deltaX * 0.05f;
+                transform.position = newPosition;
+            }
+        }
+        else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+        {
+            animator.SetBool("PrepareJump", false);
+            if (isGrounded() && !isJumpCancelled)
+            {
+                rigidBody.AddForce(Vector3.up * 10, ForceMode.VelocityChange);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/MainCameraControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MainCameraControl : MonoBehaviour
+{
+    public GameObject character;
+    public Vector3 basePosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        basePosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 characterPosition = character.transform.position;
+        Vector3 newPosition = new Vector3(basePosition.x, basePosition.y, basePosition.z);
+        newPosition.x += characterPosition.x;
+        newPosition.z += characterPosition.z;
+        transform.position = newPosition;
+    }
+}
