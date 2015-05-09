3ee69f3d6a833b1d2844e274ef8d08a448f85cd1
slightair <arksutite@gmail.com>
2015-05-09 15:32:32 +0900

    Add scene transition

---
+++ Assets/Scripts/TitleSceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TitleSceneController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTapToStartButton()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        Application.LoadLevel("DebugStage");
+    }
+}
